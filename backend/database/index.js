f9113db14d75492ebea282a3c9a32268603fc3de
doflamingo94 <khaledkhaled94400@hotmail.fr>
2023-12-24 23:50:51 +0100

    connexion-bdd

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USERNAME,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASSWORD
+    host: '35.180.32.103',
+    user: 'root',
+    database: 'blueDesk',
+    password: 'Simplon16',
+    port: 3306
 });
 
 module.exports = pool.promise();
