e6b479dec19c70e11df201d8967ed9996b18b8e1
doflamingo94 <khaledkhaled94400@hotmail.fr>
2023-12-24 14:52:04 +0100

    test-server-port

--- backend/index.js
+++ backend/index.js
@@ -26,5 +26,5 @@ app.get('/', (req, res) => res.json('Salut Khaled'))
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log("Server running....");
+    console.log("Server running on port 3001");
 })
