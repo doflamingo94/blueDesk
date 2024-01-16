b2ca4154f6e8ab00555847d4aa011f8a2b725d9b
doflamingo94 <khaledkhaled94400@hotmail.fr>
2024-01-16 22:10:16 +0100

    test-env

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,14 +1,10 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USERNAME,
-    // database: process.env.DB_NAME,
-    // password: process.env.DB_PASSWORD
-    host: '35.180.32.103',
-    user: 'root',
-    database: 'blueDesk',
-    password: 'Simplon16',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
     port: 3306
 });
 
