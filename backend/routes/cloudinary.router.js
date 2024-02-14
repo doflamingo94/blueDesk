d29177d387648827c1b8d91bdcff4b9541bc35a0
doflamingo94 <khaledkhaled94400@hotmail.fr>
2024-02-14 23:43:03 +0100

    test-3

--- backend/routes/cloudinary.router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const cloudinaryApi = require("../cloudinary/deleteFiles");
-
-router.post("/deleteFile", cloudinaryApi.deleteFile);
-module.exports = router;
